09f1f2da253ad28fd3077ddc6de9564f8be15743
Ozan Sozuoz <ozansozuoz@gmail.com>
2023-02-27 23:20:00 -0600

    update meta desc

--- app/head.tsx
+++ app/head.tsx
@@ -2,9 +2,9 @@ export default function Head() {
   return (
     <>
       <title>Ozan's Portfolio</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="ozan sozuoz portfolio" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta content='width=device-width, initial-scale=1' name='viewport' />
+      <meta name='description' content='Ozan Sozuoz Portfolio' />
+      <link rel='icon' href='/favicon.ico' />
     </>
-  )
+  );
 }
